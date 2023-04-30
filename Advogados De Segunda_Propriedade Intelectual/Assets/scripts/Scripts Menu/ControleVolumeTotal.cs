6880ec19fc357556692453f8fc3437ec6c6ff3cd
Carlos Henrique de Souza Pinto <chspcontato@gmail.com>
2023-04-30 17:29:13 -0300

    Volume funcionando

--- Advogados De Segunda_Propriedade Intelectual/Assets/scripts/Scripts Menu/ControleVolumeTotal.cs
+++ Advogados De Segunda_Propriedade Intelectual/Assets/scripts/Scripts Menu/ControleVolumeTotal.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 public class ControleVolumeTotal : MonoBehaviour
 {
     float volumeMaster;
-    
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void VolumeMaster(float volume)
     {
-        
+        volumeMaster = volume;
+        AudioListener.volume = volumeMaster;
     }
 }
